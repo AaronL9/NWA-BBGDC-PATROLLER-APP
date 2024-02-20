f1d2a6f990a5e375f6568d71a50efbbac65afc44
Aaron Lomibao <aaron.lomibao09@gmail.com>
2024-02-20 11:10:47 +0800

    added configuration

---
+++ app.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  expo: {
+    name: "mobile",
+    slug: "patroller-app",
+    version: "1.0.2",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "native",
+      backgroundColor: "#ffffff",
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      package: "com.aaron01.mobilePatroll",
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+      config: {
+        googleMaps: {
+          apiKey: process.env.EXPO_PUBLIC_API_KEY,
+        },
+      },
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    plugins: [["expo-notifications"]],
+    extra: {
+      eas: {
+        projectId: "0ef8837e-ada4-451f-ad4d-138cc77933b4",
+      },
+    },
+    owner: "aaron01",
+  },
+};
